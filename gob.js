17a2ebe98f44b24e2ac59c8d225022e9e6582daa
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-10-14 12:26:50 +0900

    Create gob.js

---
+++ gob.js
@@ -0,0 +1,15 @@
+/*
+goblin macro
+#start
+$00 = gob.setjumpback()
+#start.loop
+k>
+w>
+$wk=console.log('xyz') //one line javascript world
+{{{js
+ //multiple javascript world
+ console.log($k)
+}}}
+
+{1}>>>#start.loop //if jump
+*/
