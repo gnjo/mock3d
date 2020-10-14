932e6f461190d3b0ab1277f03f474e0aa958d8e3
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-10-14 16:17:22 +0900

    Update mock3d.js

--- mock3d.js
+++ mock3d.js
@@ -1,4 +1,5 @@
 //silence is golden
 export {a} from "./lib/a.js"
 export {b} from "./lib/b.js"
+export {lab} from "./lib/lab.js"
 //https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/export
